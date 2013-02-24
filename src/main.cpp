0e9563c8c89006f79bbc694188f7ed9fcf8a9f7c
Robert Scott Dionne <robertsdionne@gmail.com>
2013-02-24 15:01:57 -0500

    Update project.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,12 @@
 #include "ofMain.h"
-#include "testApp.h"
 #include "ofAppGlutWindow.h"
+#include "texttennis.h"
 
-//========================================================================
-int main( ){
+constexpr int kWindowWidth = 1024;
+constexpr int kWindowHeight = 768;
 
-    ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp( new testApp());
-
+int main() {
+  ofAppGlutWindow window;
+  ofSetupOpenGL(&window, kWindowWidth, kWindowHeight, OF_WINDOW);
+  ofRunApp(new TextTennis());
 }
---
+++ src/texttennis.cpp
@@ -0,0 +1,49 @@
+#include "texttennis.h"
+
+void TextTennis::setup() {
+
+}
+
+void TextTennis::update() {
+
+}
+
+void TextTennis::draw() {
+
+}
+
+void TextTennis::keyPressed(int key) {
+
+}
+
+void TextTennis::keyReleased(int key) {
+
+}
+
+void TextTennis::mouseMoved(int x, int y) {
+
+}
+
+void TextTennis::mouseDragged(int x, int y, int button) {
+
+}
+
+void TextTennis::mousePressed(int x, int y, int button) {
+
+}
+
+void TextTennis::mouseReleased(int x, int y, int button) {
+
+}
+
+void TextTennis::windowResized(int w, int h) {
+
+}
+
+void TextTennis::gotMessage(ofMessage msg) {
+
+}
+
+void TextTennis::dragEvent(ofDragInfo dragInfo) { 
+
+}
