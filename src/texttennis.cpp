3280959f923426fd362de9558e85f97aa919c310
Robert Scott Dionne <robertsdionne@gmail.com>
2013-03-03 21:29:36 -0500

    More cleanup.

--- src/texttennis.cpp
+++ src/texttennis.cpp
@@ -2,18 +2,87 @@
 
 #include "texttennis.h"
 
-const b2Vec2 TextTennis::kGravityVector = b2Vec2(0.0, -kGravity);
-const ofMatrix4x4 TextTennis::kViewMatrix = ofMatrix4x4::newScaleMatrix(kWindowWidth / kCourtLength, -kWindowWidth / kCourtLength, 1) * ofMatrix4x4::newTranslationMatrix(kWindowWidth / 2.0, kWindowHeight, 0.0);
-const ofMatrix4x4 TextTennis::kViewMatrixInverse = ofMatrix4x4::getInverseOf(kViewMatrix);
+/**
+ * Public constant definitions.
+ */
+constexpr int TextTennis::kHalfWindowWidth;
+
 constexpr int TextTennis::kWindowWidth;
+
 constexpr int TextTennis::kWindowHeight;
 
+/**
+ * Private constant definitions.
+ */
+constexpr int TextTennis::OF_KEY_TAB;
+
+constexpr float TextTennis::kAngularDamping;
+
+constexpr float TextTennis::kBallCartoonFactor;
+
+constexpr float TextTennis::kBallMass;
+
+constexpr float TextTennis::kBallRadius;
+
+constexpr int TextTennis::kBox2dVelocityIterations;
+
+constexpr int TextTennis::kBox2dPositionIterations;
+
+constexpr float TextTennis::kCourtLength;
+
+constexpr float TextTennis::kCourtThickness;
+
+constexpr float TextTennis::kDefaultAngle;
+
+constexpr float TextTennis::kDefaultAngularVelocity;
+
+constexpr float TextTennis::kDeltaTime;
+
+constexpr float TextTennis::kDrag;
+
+constexpr float TextTennis::kFrameRate;
+
+constexpr float TextTennis::kGravity;
+
+const b2Vec2 TextTennis::kGravityVector = b2Vec2(0.0, -kGravity);
+
+constexpr float TextTennis::kHalfCourtLength;
+
+constexpr float TextTennis::kHitMean;
+
+constexpr float TextTennis::kHitVariance;
+
+constexpr float TextTennis::kLinearDamping;
+
+constexpr long TextTennis::kMaxBalls;
+
+constexpr float TextTennis::kNetHeight;
+
+constexpr float TextTennis::kNetThickness;
+
+const ofVec2f TextTennis::kRacket1StartPosition = ofVec2f(-8, kCourtThickness + kRacketRadius);
+
+constexpr float TextTennis::kRacketRadius;
+
+constexpr float TextTennis::kRacketSpeed;
+
+constexpr int TextTennis::kTrailSize;
+
+const ofMatrix4x4 TextTennis::kViewMatrix =
+    ofMatrix4x4::newScaleMatrix(kWindowWidth / kCourtLength, -kWindowWidth / kCourtLength, 1) *
+        ofMatrix4x4::newTranslationMatrix(kHalfWindowWidth, kWindowHeight, 0.0);
+
+const ofMatrix4x4 TextTennis::kViewMatrixInverse = ofMatrix4x4::getInverseOf(kViewMatrix);
+
+/**
+ * Public method definitions.
+ */
 TextTennis::TextTennis()
 : console(), world(kGravityVector), ball_body(), ball_shape(), ball_fixture(nullptr),
   court_body(nullptr), court_shape(), court_fixture(nullptr), net_body(nullptr), net_shape(),
   net_fixture(nullptr), border_body(nullptr), border_shape(), border_fixture(nullptr),
   show_console(false), show_text(false), use_ai(false),
-  racket1(-8, kCourtThickness + kRacketRadius), mouse_position(), states(), previous_keys(), keys()
+  racket1(kRacket1StartPosition), mouse_position(), states(), previous_keys(), keys()
 {}
 
 void TextTennis::setup() {
@@ -21,6 +90,7 @@ void TextTennis::setup() {
   ofSetVerticalSync(true);
   ofEnableAlphaBlending();
   ofEnableSmoothing();
+  ofBackground(ofColor::white);
 
   // Box2D
   CreateBall(ofVec2f(11, 1), ofVec2f(-15, 7.5));
@@ -38,12 +108,12 @@ void TextTennis::setup() {
 }
 
 void TextTennis::update() {
-  if (keys['\t']) {
+  if (keys[OF_KEY_TAB]) {
     if (states.size() > 1) {
       states.pop_back();
     }
   } else {
-    if (!keys['\t'] && previous_keys['\t']) {
+    if (!keys[OF_KEY_TAB] && previous_keys[OF_KEY_TAB]) {
       racket1 = states.back().racket1;
       for (auto body : ball_body) {
         body->DestroyFixture(body->GetFixtureList());
@@ -83,29 +153,17 @@ void TextTennis::update() {
 
 void TextTennis::draw() {
   ofMultMatrix(kViewMatrix);
-  ofBackground(ofColor::white);
-  ofSetColor(ofColor::black);
-  ofRect(ofVec2f(-kCourtLength / 2.0, kCourtThickness), kCourtLength, -kCourtThickness);
-  ofRect(ofVec2f(-kNetThickness / 2.0, kNetHeight + kCourtThickness), kNetThickness, -kNetHeight);
-  if (keys['\t']) {
-    ofCircle(states.back().racket1, kRacketRadius);
+  DrawCourt();
+  DrawNet();
+  if (keys[OF_KEY_TAB]) {
+    DrawRacket(states.back().racket1);
     for (auto ball : states.back().balls) {
-      ofSetColor(ofColor::black);
-      ofCircle(ball.position, kBallRadius);
-      ofSetColor(ofColor::white);
-      ofLine(ball.position,
-             ball.position + kBallRadius * ofVec2f(cos(ball.angle), sin(ball.angle)));
+      DrawBall(ball.position, ball.angle);
     }
   } else {
-    ofCircle(racket1, kRacketRadius);
+    DrawRacket(racket1);
     for (auto ball : ball_body) {
-      const float angle = ball->GetAngle();
-      const b2Vec2 position = ball->GetPosition();
-      ofSetColor(ofColor::black);
-      ofCircle(ofVec2f(position.x, position.y), kBallRadius);
-      ofSetColor(ofColor::white);
-      ofLine(ofVec2f(position.x, position.y),
-             ofVec2f(position.x, position.y) + kBallRadius * ofVec2f(cos(angle), sin(angle)));
+      DrawBall(ofVec2f(ball->GetPosition().x, ball->GetPosition().y), ball->GetAngle());
     }
   }
   std::stringstream out;
@@ -131,6 +189,9 @@ void TextTennis::mouseMoved(int x, int y) {
   mouse_position = ofVec3f(x, y) * kViewMatrixInverse;
 }
 
+/**
+ * Private method defintions.
+ */
 void TextTennis::CreateBall(ofVec2f position, ofVec2f velocity,
                             float angle, float angular_velocity) {
   b2BodyDef ball_body_definition;
@@ -185,6 +246,36 @@ void TextTennis::CreateNet() {
   b2FixtureDef net_fixture_definition;
   net_fixture_definition.shape = &net_shape;
   net_fixture = net_body->CreateFixture(&net_fixture_definition);
+}
+
+void TextTennis::DrawBall(ofVec2f position, float angle) {
+  ofPushStyle();
+  ofSetColor(ofColor::black);
+  ofCircle(position, kBallRadius);
+  ofSetColor(ofColor::white);
+  ofLine(position, position + kBallRadius * ofVec2f(cos(angle), sin(angle)));
+  ofPopStyle();
+}
+
+void TextTennis::DrawCourt() {
+  ofPushStyle();
+  ofSetColor(ofColor::black);
+  ofRect(ofVec2f(-kHalfCourtLength, kCourtThickness), kCourtLength, -kCourtThickness);
+  ofPopStyle();
+}
+
+void TextTennis::DrawNet() {
+  ofPushStyle();
+  ofSetColor(ofColor::black);
+  ofRect(ofVec2f(-kNetThickness / 2.0, kNetHeight + kCourtThickness), kNetThickness, -kNetHeight);
+  ofPopStyle();
+}
+
+void TextTennis::DrawRacket(ofVec2f position) {
+  ofPushStyle();
+  ofSetColor(ofColor::black);
+  ofCircle(position, kRacketRadius);
+  ofPopStyle();
 }
 
 void TextTennis::RacketCollide() {
--- src/texttennis.h
+++ src/texttennis.h
@@ -44,7 +44,9 @@ public:
   /**
    * Public constant declarations.
    */
-  static constexpr int kWindowWidth = 1024;
+  static constexpr int kHalfWindowWidth = 512;
+  
+  static constexpr int kWindowWidth = 2 * kHalfWindowWidth;
   
   static constexpr int kWindowHeight = 768;
   
@@ -52,32 +54,62 @@ private:
   /**
    * Private constant declarations.
    */
+  static constexpr int OF_KEY_TAB = '\t';
+  
   static constexpr float kAngularDamping = 0.1;
+  
   static constexpr float kBallCartoonFactor = 2.0;
+  
   static constexpr float kBallMass = 0.056;
+  
   static constexpr float kBallRadius = kBallCartoonFactor * 0.03429;
+  
   static constexpr int kBox2dVelocityIterations = 8;
+  
   static constexpr int kBox2dPositionIterations = 3;
+  
   static constexpr float kCourtLength = 23.78;
+  
   static constexpr float kCourtThickness = 0.5;
+  
   static constexpr float kDefaultAngle = 0.0;
+  
   static constexpr float kDefaultAngularVelocity = 0.0;
+  
   static constexpr float kDeltaTime = 1.0 / 60.0;
+  
   static constexpr float kDrag = 0.1 * kBallMass;
+  
   static constexpr float kFrameRate = 1.0 / kDeltaTime;
+  
   static constexpr float kGravity = 9.81;
+  
   static const b2Vec2 kGravityVector;
+  
   static constexpr float kHalfCourtLength = kCourtLength / 2.0;
+  
   static constexpr float kHitMean = 0.20;
+  
   static constexpr float kHitVariance = 0.08;
+  
   static constexpr float kLinearDamping = 0.1;
+  
   static constexpr long kMaxBalls = 500;
+  
   static constexpr float kNetHeight = 0.914;
+  
   static constexpr float kNetThickness = kBallRadius;
+  
+  static const ofVec2f kRacket1StartPosition;
+  
   static constexpr float kRacketRadius = kBallCartoonFactor * 0.1155;
+  
   static constexpr float kRacketSpeed = 0.1;
+  
   static constexpr int kTrailSize = 700;
+  
   static const ofMatrix4x4 kViewMatrix;
+  
   static const ofMatrix4x4 kViewMatrixInverse;
   
 private:
@@ -95,6 +127,12 @@ private:
   
   void DrawBall(ofVec2f position, float angle);
   
+  void DrawCourt();
+  
+  void DrawNet();
+  
+  void DrawRacket(ofVec2f position);
+  
   void RacketCollide();
   
   void UpdateRackets();
@@ -105,33 +143,54 @@ private:
    */
   Console console;
   
+  
   b2World world;
   
+  
   std::list<b2Body *> ball_body;
+  
   b2CircleShape ball_shape;
+  
   b2Fixture *ball_fixture;
   
+  
   b2Body *court_body;
+  
   b2PolygonShape court_shape;
+  
   b2Fixture *court_fixture;
   
+  
   b2Body *net_body;
+  
   b2EdgeShape net_shape;
+  
   b2Fixture *net_fixture;
   
+  
   b2Body *border_body;
+  
   b2ChainShape border_shape;
+  
   b2Fixture *border_fixture;
   
+  
   bool show_console;
+  
   bool show_text;
+  
   bool use_ai;
   
+  
   ofVec2f racket1;
+  
   ofVec2f mouse_position;
+  
   
   std::list<GameState> states;
   
+  
   std::map<int, bool> previous_keys;
+  
   std::map<int, bool> keys;
 };
